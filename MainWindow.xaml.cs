9e6126f0e11752dc8506b9643b3e8c0e207b7d35
samsulhuda76 <61945288+samsulhuda76@users.noreply.github.com>
2020-11-18 21:08:38 +0700

    FreshFruit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using FreshFruit.Controller;
+using FreshFruit.Model;
+using FreshFuit.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FreshFruit
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, BucketEventListener
+    {
+        Seller toni;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Bucket keranjangBuah = new Bucket(2);
+            BucketController bucketcontroller = new BucketController(keranjangBuah, this);
+
+            toni = new Seller("Toni", bucketcontroller);
+
+            listBoxBucket.ItemsSource = keranjangBuah.findAll();
+        }
+
+        public void onFailed(string message)
+        {
+            MessageBox.Show(message);
+        }
+
+        public void onSucceed(string message)
+        {
+            listBoxBucket.Items.Refresh();
+        }
+
+        private void OnButtonAddAnggurClicked(object sender, RoutedEventArgs e)
+        {
+            Fruit anggur = new Fruit("Anggur");
+            toni.addFruit(anggur);
+
+            listBoxBucket.Items.Refresh();
+        }
+
+        private void OnButtonAddAppleClicked(object sender, RoutedEventArgs e)
+        {
+            Fruit apple = new Fruit("Apple");
+            toni.addFruit(apple);
+
+            listBoxBucket.Items.Refresh();
+        }
+
+        private void OnButtonAddBananaClicked(object sender, RoutedEventArgs e)
+        {
+            Fruit banana = new Fruit("Banana");
+            toni.addFruit(banana);
+
+            listBoxBucket.Items.Refresh();
+        }
+
+        private void OnButtonAddOrangeClicked(object sender, RoutedEventArgs e)
+        {
+            Fruit orange = new Fruit("Orange");
+            toni.addFruit(orange);
+
+            listBoxBucket.Items.Refresh();
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,45 @@
+﻿# Fresh Fruit
+Aplikasi ini dapat digunakan oleh user dalam menambahkan maupun menghapus sebuah item yang berupa buah ke dalam keranjang.
+# Scope & Functionailities
+- User dapat menambahkan buah yang akan di pilih
+- User dapat menghapus buah yang telah di pilih
+- User akan mendapatkan sebuah notifikasi apabila keranjang telah penuh
+
+## How Does it Works
+Diawali method pada class `Bucket.cs` dengan mendeklarasikan :
+```csharp
+class Bucket
+    {
+        private int capacity;
+        private List<Fruit> fruits;
+
+        public Bucket(int capacity)
+        {
+            this.capacity = capacity;
+            this.fruits = new List<Fruit>();
+        }
+
+        public void insert(Fruit fruit)
+        {
+            this.fruits.Add(fruit);
+        }
+
+        public void remove(int position)
+        {
+            this.fruits.RemoveAt(position);
+        }
+
+        public List<Fruit> findAll()
+        {
+            return this.fruits;
+        }
+        public int getCapacity()
+        {
+            return this.capacity;
+        }
+        public int countItems()
+        {
+            return this.fruits.Count();
+        }
+    }
+```
